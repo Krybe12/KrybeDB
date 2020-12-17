dded91074c623ba5883ba1d4498531f48426b933
Krybe12 <Krybe120@gmail.com>
2020-12-17 09:01:38 +0100

    add mathgame achievments

--- matgame/gettotal.php
+++ matgame/gettotal.php
@@ -13,4 +13,33 @@ if (isset($_SESSION["mats"])){
     $_SESSION["mats"] = $result["score"];
     echo "Total Score: " . $_SESSION["mats"];
 }
+if ($_SESSION["mats"] > 0 and !isset($_SESSION["achdone"][3])){
+    achiev(3); //achievement first priklad trigger
+}
+if ($_SESSION["mats"] >= 10 and !isset($_SESSION["achdone"][4])){
+    achiev(4); //achievement 10 vyocitanejch trigger
+}
+if ($_SESSION["mats"] >= 50 and !isset($_SESSION["achdone"][5])){
+    achiev(5); //achievement 50 vyocitanejch trigger
+}
+if ($_SESSION["mats"] >= 100 and !isset($_SESSION["achdone"][6])){
+    achiev(6); //achievement 100 vyocitanejch trigger
+}
+if ($_SESSION["mats"] >= 250 and !isset($_SESSION["achdone"][7])){
+    achiev(7); //achievement 250 vyocitanejch trigger
+}
+
+function achiev($achid){
+    global $conn;      
+    $sql = "INSERT INTO achcompleted (user_id, ach_id) VALUES ({$_SESSION['userid']}, $achid)";
+    $conn->query($sql);
+    $_SESSION["achdone"][$achid] = 1;
+    echo '<script>
+    $(document).ready(function(){
+        $.get( "../achievements/load.php", { achid:' . $achid . '}, function(data){
+            $(".alerty").append(data);
+        });
+    });   
+    </script>';
+}
 ?>
